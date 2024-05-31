eb5b9ee394f871ec8f4c0907f9d2d917f77288f5
reqsanemustafayevaa <146015080+reqsanemustafayevaa@users.noreply.github.com>
2024-05-31 16:52:30 +0400

    Looptask

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Bir proqram yazın ki, istifadəçinin daxil etdiyi rəqəmin faktorialını hesablasın.(while ilə)
+                Console.WriteLine("Please enter the number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            while (a > 0)
+            {
+                fact *= a;
+                a--;
+            }
+            Console.WriteLine($" ededin faktoriali {fact}");
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
